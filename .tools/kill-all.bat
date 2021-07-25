85da4be7a07def1920ee02bc4113890cb9c8c4a8
kevingrillet <kevin.grillet@gmail.com>
2021-07-25 15:33:46 +0200

    Quick update

---
+++ .tools/kill-all.bat
@@ -0,0 +1,14 @@
+:: Kills everything related to AFK-Daily
+
+:: Bluestacks 4
+taskkill /F /IM Bluestacks.exe
+
+:: Bluestacks 4 & 5
+taskkill /F /IM HD-Player.exe
+taskkill /F /IM BstkSVC.exe
+
+:: ADB
+taskkill /F /IM adb.exe
+
+:: Shell
+taskkill /F /IM bash.exe
