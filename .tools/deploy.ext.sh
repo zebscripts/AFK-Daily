5f4a874b9c871374bc59abdc23961ce48ddf16fb
kevingrillet <kevin.grillet@gmail.com>
2021-09-03 20:19:56 +0200

    Create deploy.ext.sh

---
+++ .tools/deploy.ext.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+# ##############################################################################
+# Script Name   : deploy.ext.sh
+# Description   : Extension of the deploy script, used for backup & tools
+# GitHub        : https://github.com/zebscripts/AFK-Daily
+# License       : MIT
+# ##############################################################################
