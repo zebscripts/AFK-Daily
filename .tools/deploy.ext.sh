6f9c024a49fe8662cf10687aefbc98847bb3da4e
Kévin GRILLET <kevin@MacBook-Pro-de-Kevin.local>
2021-10-10 10:05:59 +0200

    Fix Optimize code related to generating and checking files
    Remove validateConfig
    
    checkConfig first! (create file + msg + warn)

--- .tools/deploy.ext.sh
+++ .tools/deploy.ext.sh
@@ -5,3 +5,68 @@
 # GitHub        : https://github.com/zebscripts/AFK-Daily
 # License       : MIT
 # ##############################################################################
+
+# ##############################################################################
+# Function Name : validateConfig
+# Description   : Checks for every necessary variable that needs to be defined in $configFile
+# ##############################################################################
+validateConfig() {
+    source $configFile
+    printTask "Validating ${cCyan}$configFile${cNc}..."
+    if [[ -z $canOpenSoren || -z \
+        $arenaHeroesOpponent || -z \
+        $waitForUpdate || -z \
+        $endAt || -z \
+        $maxCampaignFights || -z \
+        $maxKingsTowerFights || -z \
+        $totalAmountArenaTries || -z \
+        $totalAmountTournamentTries || -z \
+        $totalAmountGuildBossTries || -z \
+        $totalAmountTwistedRealmBossTries || -z \
+        $buyStoreDust || -z \
+        $buyStorePoeCoins || -z \
+        $buyStorePrimordialEmblem || -z \
+        $buyStoreAmplifyingEmblem || -z \
+        $buyStoreSoulstone || -z \
+        $buyStoreLimitedGoldOffer || -z \
+        $buyStoreLimitedDiamOffer || -z \
+        $buyWeeklyGuild || -z \
+        $buyWeeklyLabyrinth || -z \
+        $doMainTower || -z \
+        $doTowerOfLight || -z \
+        $doTheBrutalCitadel || -z \
+        $doTheWorldTree || -z \
+        $doCelestialSanctum || -z \
+        $doTheForsakenNecropolis || -z \
+        $doInfernalFortress || -z \
+        $doLootAfkChest || -z \
+        $doChallengeBoss || -z \
+        $doFastRewards || -z \
+        $doCollectFriendsAndMercenaries || -z \
+        $doSoloBounties || -z \
+        $doTeamBounties || -z \
+        $doArenaOfHeroes || -z \
+        $doLegendsTournament || -z \
+        $doKingsTower || -z \
+        $doGuildHunts || -z \
+        $guildBattleType || -z \
+        $doTwistedRealmBoss || -z \
+        $doBuyFromStore || -z \
+        $doStrengthenCrystal || -z \
+        $allowCrystalLevelUp || -z \
+        $doTempleOfAscension || -z \
+        $doCompanionPointsSummon || -z \
+        $doCollectOakPresents || -z \
+        $doCollectQuestChests || -z \
+        $doCollectMail || -z \
+        $doCollectMerchantFreebies ]]; then
+        printError "${cCyan}$configFile${cNc} has missing/wrong entries."
+        echo
+        printInfo "Please either delete ${cCyan}$configFile${cNc} and run the script again to generate a new one,"
+        printInfo "or run ./lib/update_setup.sh -c"
+        printInfo "or check the following link for help:"
+        printInfo "https://github.com/zebscripts/AFK-Daily/wiki/Config"
+        exit
+    fi
+    printSuccess "Passed!"
+}
--- deploy.sh
+++ deploy.sh
@@ -210,8 +210,6 @@ doCollectMerchantFreebies=false
         printInfo "Please edit ${cCyan}$configFile${cNc} if necessary and run this script again."
         exit
     fi
-
-    validateConfig # Validate config file
 }
 
 # ##############################################################################
@@ -521,71 +519,6 @@ lastWeekly=${newLastWeekly:-$lastWeekly}" >"$tempFile"
 }
 
 # ##############################################################################
-# Function Name : validateConfig
-# Description   : Checks for every necessary variable that needs to be defined in $configFile
-# ##############################################################################
-validateConfig() {
-    source $configFile
-    printTask "Validating ${cCyan}$configFile${cNc}..."
-    if [[ -z $canOpenSoren || -z \
-        $arenaHeroesOpponent || -z \
-        $waitForUpdate || -z \
-        $endAt || -z \
-        $maxCampaignFights || -z \
-        $maxKingsTowerFights || -z \
-        $totalAmountArenaTries || -z \
-        $totalAmountTournamentTries || -z \
-        $totalAmountGuildBossTries || -z \
-        $totalAmountTwistedRealmBossTries || -z \
-        $buyStoreDust || -z \
-        $buyStorePoeCoins || -z \
-        $buyStorePrimordialEmblem || -z \
-        $buyStoreAmplifyingEmblem || -z \
-        $buyStoreSoulstone || -z \
-        $buyStoreLimitedGoldOffer || -z \
-        $buyStoreLimitedDiamOffer || -z \
-        $buyWeeklyGuild || -z \
-        $buyWeeklyLabyrinth || -z \
-        $doMainTower || -z \
-        $doTowerOfLight || -z \
-        $doTheBrutalCitadel || -z \
-        $doTheWorldTree || -z \
-        $doCelestialSanctum || -z \
-        $doTheForsakenNecropolis || -z \
-        $doInfernalFortress || -z \
-        $doLootAfkChest || -z \
-        $doChallengeBoss || -z \
-        $doFastRewards || -z \
-        $doCollectFriendsAndMercenaries || -z \
-        $doSoloBounties || -z \
-        $doTeamBounties || -z \
-        $doArenaOfHeroes || -z \
-        $doLegendsTournament || -z \
-        $doKingsTower || -z \
-        $doGuildHunts || -z \
-        $guildBattleType || -z \
-        $doTwistedRealmBoss || -z \
-        $doBuyFromStore || -z \
-        $doStrengthenCrystal || -z \
-        $allowCrystalLevelUp || -z \
-        $doTempleOfAscension || -z \
-        $doCompanionPointsSummon || -z \
-        $doCollectOakPresents || -z \
-        $doCollectQuestChests || -z \
-        $doCollectMail || -z \
-        $doCollectMerchantFreebies ]]; then
-        printError "${cCyan}$configFile${cNc} has missing/wrong entries."
-        echo
-        printInfo "Please either delete ${cCyan}$configFile${cNc} and run the script again to generate a new one,"
-        printInfo "or run ./lib/update_setup.sh -c"
-        printInfo "or check the following link for help:"
-        printInfo "https://github.com/zebscripts/AFK-Daily/wiki/Config"
-        exit
-    fi
-    printSuccess "Passed!"
-}
-
-# ##############################################################################
 # Section       : Script Start
 # ##############################################################################
 
@@ -596,8 +529,8 @@ check_all() {
     checkFolders
     checkAdb
     if [ $doCheckGitUpdate = true ]; then checkGitUpdate; fi
-    checkSetupUpdate
     checkConfig
+    checkSetupUpdate
     checkEOL $tempFile
     checkEOL $configFile
     checkEOL "afk-daily.sh"
